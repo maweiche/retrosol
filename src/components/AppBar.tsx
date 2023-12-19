afddba916795961146b94a3d5c69e417e2b2ff36
Matt <maweiche@gmail.com>
2023-12-20 00:03:42 +0100

    fixed logo leading to homepage

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -21,7 +21,7 @@ export const AppBar: React.FC = () => {
       <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
         <div className="navbar-start align-items-center">
           <div className="hidden sm:inline w-22 h-22 md:p-2 ml-10">
-            <Link href="https://solana.com" target="_blank" rel="noopener noreferrer" passHref className="text-secondary hover:text-white flex-row">
+            <Link href="/"  passHref className="text-secondary hover:text-white flex-row">
               <Image
                 src="/RetroSolLogo.svg"
                 alt="Retro Sol Logo"
